5a41dc0a3652a52c9d65451ec0152770a63d543a
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-07-06 21:14:04 +0300

    clear:main

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import {useEffect, useState} from "react";
 
-import {Posts, SpaceX, Users} from "./components";
 import styles from "./App.module.css";
 
 function App() {
-    const [posts, setPosts] = useState([]);
-    const [owenPosts, setOwenPosts] = useState([]);
-    const [userName, setUserName] = useState('');
-    const [showPostCard, setShowPostCard] = useState(false);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(data => setPosts(data))
-            .catch(error => console.log(error))
-    }, [])
-
-    const handleClick = (user) => {
-        setOwenPosts(posts.filter(post => post.userId === user.id));
-        setUserName(user.name);
-        setShowPostCard(false);
-    }
 
     return (<div className={styles.section}>
-        <Users handleClick={handleClick}/>
-        {userName && <h2 className={styles.userName}>{userName}</h2>}
-        <Posts owenPosts={owenPosts} setShowPostCard={setShowPostCard} showPostCard={showPostCard}/>
-        <SpaceX/>
-
+        App
     </div>);
 }
 
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export {Posts} from './Posts/Posts';
-export {PostCard} from './PostCard/PostCard';
-export {Users} from './Users/Users';
-export {UserCard} from './UserCard/UserCard';
-export {SpaceX} from './SpaceX/SpaceX';
-export {SpaceXCard} from './SpaceXCard/SpaceXCard';
