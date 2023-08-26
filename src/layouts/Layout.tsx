b40f9e906f4ff6dd135c1e44b28ca3459a8a3d9c
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-08-26 20:30:41 +0300

    add: loader

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,24 +1,25 @@
-import React, { createContext } from "react";
+import {LinearProgress} from "@mui/material";
+import React, {createContext} from "react";
 import {Outlet, useLocation} from "react-router-dom";
 
 import {Header} from "../components";
 import {useAppSelector} from "../hooks/reduxHooks";
 
-export  const PathnameContext = createContext(null);
+export const PathnameContext = createContext(null);
 
 export const Layout = () => {
 
     const location = useLocation();
 
-    const {isLoading} = useAppSelector((state) => state.cars); //todo
-    console.log('isLoading===>', isLoading);
+    const {isLoading} = useAppSelector((state) => state.cars);
+    const {cars} = useAppSelector(state => state.cars)
+
     return (
         <PathnameContext.Provider value={location}>
-        <>
-            <Header/>
-            <Outlet/>
-            {/* {isLoading ? <LinearProgress /> : <Outlet />} */}
-        </>
+            <>
+                <Header/>
+                {cars.length === 0 && isLoading ? <LinearProgress/> : <Outlet/>}
+            </>
         </PathnameContext.Provider>
     );
 };
